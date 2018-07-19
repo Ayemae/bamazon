3b41dc7470b5a874c83269fb47d138d3bce0d71e
Alyssa Alecci <Alyssa@Alyssas-iMac.local>
2018-07-18 22:11:24 -0400

    ....

--- bamazon.sql
+++ bamazon.sql
@@ -10,32 +10,35 @@ CREATE TABLE products (
     department_name VARCHAR (140),
     price DECIMAL (5,2) NOT NULL,
     stock INT(10) NOT NULL,
-    PRIMARY KEY (id)
-)
+    PRIMARY KEY (item_id)
+);
 
 INSERT INTO products (product_name, department_name, price, stock)
-VALUES ("How To Fly a Kite","Books", 10.00, 25)
+VALUES ("How To Fly a Kite","Books", 10.00, 25);
 
 INSERT INTO products (product_name, department_name, price, stock)
-VALUES ("Sage's Quest","video games", 20.00, 100)
+VALUES ("Sage's Quest","video games", 20.00, 100);
 
 INSERT INTO products (product_name, department_name, price, stock)
-VALUES ("Nintendo Switch", "video games", 300.00, 10)
+VALUES ("Nintendo Switch", "video games", 300.00, 10);
 
 INSERT INTO products (product_name, department_name, price, stock)
-VALUES ("Lawn Chair", "furniture", 60.00, 10)
+VALUES ("Lawn Chair", "furniture", 60.00, 10);
 
 INSERT INTO products (product_name, department_name, price, stock)
-VALUES ("construction paper pack", "arts and crafts", 5.00, 300)
+VALUES ("construction paper pack", "arts and crafts", 5.00, 300);
 
 INSERT INTO products (product_name, department_name, price, stock)
-VALUES ("Pet Plate", "pet care", 8.00, 100)
+VALUES ("Pet Plate", "pet care", 8.00, 100);
 
 INSERT INTO products (product_name, department_name, price, stock)
-VALUES ("giant chocolate bar", "food", 6.00, 80)
+VALUES ("giant chocolate bar", "food", 6.00, 80);
 
 INSERT INTO products (product_name, department_name, price, stock)
-VALUES ("Javascript for Dummies", "book", 10.00, 100)
+VALUES ("Javascript for Dummies", "book", 10.00, 100);
 
 INSERT INTO products (product_name, department_name, price, stock)
-VALUES ("lava lamp", "furniture", 12.00, 60)
+VALUES ("lava lamp", "furniture", 12.00, 60);
+
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("glue gun", "arts and crafts", 12.00, 30);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,40 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-function getItems() {
-    connection.query("SELECT * FROM products", function (err, response) {
-        if (err) throw err;
-        for (var i = 0; i < response.length; i++) {
-            items.push(response[i]);
-        }
-    })
-}
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8888,
+    user: "root",
+    password: "root",
+    database: "bamazon_db"
+  });
+
+
+// // connect to the mysql server and sql database
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     // run the start function after the connection is made to prompt the user
+//     getItems();
+//   });
+
+// function getItems() {
+//     connection.query("SELECT item_id,product_name,price FROM products", function (err, res) {
+//         if (err) throw err;
+
+//         inquirer.prompt([
+//             {
+//                 name: "buyWhat",
+//                 type: "rawlist",
+//                 choices: function() {
+//                     var itemsArr = [];
+//                     for (var i = 0; i < res.length; i++) {
+//                         itemsArr.push(res[i].product_name);
+//                     }
+//                     return itemsArr;
+//                 },
+//                 message: "What would you like to buy?"
+//             },
+//             {}
+//         ])
+//     })
+// }
